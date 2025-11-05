2a21eaa11eb41e45b4abb09247854b74ecd2dfa8
TanvirRelisource <138788934+TanvirRelisource@users.noreply.github.com>
2025-11-05 22:08:03 +0600

    Delete tests directory

--- tests/Assertions.spec.js
+++
@@ -1,7 +0,0 @@
-import test, {page, expect} from '@playwright/test';
-
-test('Assertions Example', async ({page}) => {
-  await page.goto('https://kitchen.applitools.com/');
-  await page.pause();
-});
-
--- tests/my_first_test.spec.js
+++
@@ -1,8 +0,0 @@
-const { test, expect } = require('@playwright/test');
-
-test('My First Test', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
